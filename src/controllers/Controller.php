029f457f0490c050b9566fc2ca8dce5dd4df9afb
Tania Rascia <taniarascia@gmail.com>
2018-06-14 09:43:23 -0500

    Clean up

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -5,14 +5,12 @@ abstract class Controller
     protected $page_title;
     protected $message;
 
-    // Show the view
     protected function view($view) {
         $view = strtolower($view);
 
         require_once $_SERVER['DOCUMENT_ROOT'] . '/../src/views/' . $view . '.view.php';
     }
 
-    // Set new header location
     protected function redirect($view) {
         $view = strtolower($view);
 
@@ -20,7 +18,6 @@ abstract class Controller
         exit;
     }
 
-    // Is user logged in
     protected function authenticate() {
         if (!isset($_SESSION['user_id']) || !isset($_SESSION['is_logged_in'])) {
             //User not logged in. Redirect them back to the login.php page.
@@ -42,14 +39,12 @@ abstract class Controller
         session_destroy();
     }
 
-    // Hash password
     protected function encryptPassword($password) {
         $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
         
         return $password;
     }
 
-    // Verify password
     protected function verifyPassword($submittedPassword, $password) {
         $validPassword = password_verify($submittedPassword, $password);
         
