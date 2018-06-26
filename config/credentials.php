1a88182b7ce6c466e2765997608f45369479d5a4
Tania Rascia <taniarascia@gmail.com>
2018-06-26 10:35:11 -0500

    Additional commenting on functions

--- config/credentials.php
+++ config/credentials.php
@@ -1,5 +1,6 @@
 <?php
 
+// Database credentials
 define('DB_HOST', 'localhost');  
 define('DB_USER', 'root');  
 define('DB_PASS', 'root');  
--- config/utility.php
+++ config/utility.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Get controller name, which is PascalCase, from URLs, which
+ * are kebab-case.
+ * Return a string.
+ */
+
 function getControllerName($path) {
     $path = ltrim($path, '/');                                        // Strip trailing whitespace
     $path = ucfirst($path);                                           // Capitalize first letter
@@ -9,12 +15,22 @@ function getControllerName($path) {
     return $path;                      
 }
 
+/**
+ * Get filtered $_POST values.
+ * Return an array.
+ */
+
 function filter_post() {
     $post = filter_input_array(INPUT_POST);
     $post = array_map('trim', $post);
     
     return $post;
 }
+
+/**
+ * Get filtered $_GET values.
+ * Return an array.
+ */
 
 function filter_get() {
     $get = filter_input_array(INPUT_GET);
