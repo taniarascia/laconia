61a81533a1bdcf9dc285b2455171e3c4ceecf3c7
root <kevin.schmitt.upjv@gmail.com>
2019-08-13 20:37:54 +0200

    fix: docker-compose database name not correspond to name fo database incredential

--- config/credentials_back.php
+++ config/credentials_back.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database credentials for docker 
-define('DB_HOST', 'localhost');  
+define('DB_HOST', 'database');  
 define('DB_USER', 'root');  
 define('DB_PASS', 'root');  
 define('DB_NAME', 'laconia');  
