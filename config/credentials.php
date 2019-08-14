2215528580bb6cc4fef3727719ea5cd66dc6a6d0
root <kevin.schmitt.upjv@gmail.com>
2019-08-14 04:35:55 +0200

    fix: delete white space

--- config/credentials.php
+++ config/credentials.php
@@ -1,7 +1,6 @@
 <?php
-
-// Database credentials 
-define('DB_HOST', 'localhost');  
-define('DB_USER', 'root');  
-define('DB_PASS', 'root');  
-define('DB_NAME', 'laconia');  
+// Authentication credentials
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'laconia');
